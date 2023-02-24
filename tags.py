dbf617ba5a4e97d1f1558000d1918c07241c1b38
siva143sp <42701505+siva143sp@users.noreply.github.com>
2023-02-24 07:50:54 +0530

    Add files via upload
    This is the python script which can read the csv file and apply the tags.

---
+++ tags.py
@@ -0,0 +1,35 @@
+import boto3
+import csv
+import os
+os.environ['AWS_PROFILE'] = "736176406297-ADFS-MHF-EKSadmin"
+os.environ['AWS_DEFAULT_REGION'] = "us-east-1"
+session = boto3.session.Session(profile_name='736176406297-ADFS-MHF-EKSadmin')
+client = session.client('s3', 'us-east-1')
+s3 = boto3.client('s3', region_name='us-east-1')
+
+
+# Read bucket names and tags from CSV file
+with open('tags.csv') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        bucket_name = row['Bucket Name']
+        new_tags = []
+        for key, value in row.items():
+            if key != 'Bucket' and value:
+                new_tags.append({'Key': key, 'Value': value})
+
+        # Get existing tags for the bucket
+        try:
+            existing_tags = s3.get_bucket_tagging(Bucket=bucket_name)['TagSet']
+        except:
+            # Bucket does not have tags yet
+            existing_tags = []
+
+        # Merge existing and new tags
+        merged_tags = list(existing_tags)
+        for tag in new_tags:
+            if tag not in merged_tags:
+                merged_tags.append(tag)
+
+        # Update bucket tags
+        s3.put_bucket_tagging(Bucket=bucket_name, Tagging={'TagSet': merged_tags})
